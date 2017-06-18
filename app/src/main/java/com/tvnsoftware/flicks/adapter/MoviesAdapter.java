b5a5182813875da90943f2860039982b4a64ccad
Thieusike-Pro\Thieusike <Tam101901@gmail.com>
2017-06-19 02:32:09 +0700

    add fade in when apperance of list main

--- app/src/main/java/com/tvnsoftware/flicks/adapter/MoviesAdapter.java
+++ app/src/main/java/com/tvnsoftware/flicks/adapter/MoviesAdapter.java
@@ -8,6 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -32,6 +34,7 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 public class MoviesAdapter extends ArrayAdapter<Movie> {
 
     private MoviesListener mListener;
+    boolean[] animationStates;
 
     public interface MoviesListener {
         public void onItemClick(Movie movie);
@@ -40,6 +43,7 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
     public MoviesAdapter(Context context, MoviesListener listener) {
         super(context, 0);
         mListener = listener;
+
     }
 
     static class ViewHolder {
@@ -63,11 +67,19 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        animationStates = new boolean[getCount()];
         final Movie movie = getItem(position);
         ViewHolder viewHolder;
         if (null == convertView) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            if (!animationStates[position]) {
+
+                animationStates[position] = true;
+                Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+                animation.setStartOffset(position * 500);
+                convertView.startAnimation(animation);
+            }
             viewHolder = new ViewHolder(convertView);
             convertView.setTag(viewHolder);
         } else {
